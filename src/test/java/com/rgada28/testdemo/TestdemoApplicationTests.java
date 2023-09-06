48a92d3a7b7b64c62e8939a9613e012951f526fb
Raj Gada <rgada28@gmail.com>
2023-09-06 11:02:03 +0530

    Initial commit

---
+++ src/test/java/com/rgada28/testdemo/TestdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rgada28.testdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
